5accfa9179601b6bd64fcdf01f3eda90bd3a62d4
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-08-09 17:45:32 +0300

    adding new assets now works

--- src/screens/AddNewAssetScreen/index.jsx
+++ src/screens/AddNewAssetScreen/index.jsx
@@ -4,7 +4,9 @@ import SearchableDropdown from "react-native-searchable-dropdown";
 import styles from "./styles";
 import { useRecoilState } from "recoil";
 import { allPortfolioBoughtAssetsInStorage } from "../../atoms/PortfolioAssets";
-import { getAllCoins } from "../../services/requests";
+import { getAllCoins, getDetailedCoinData } from "../../services/requests";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 const AddNewAssetScreen = () => {
   const [allCoins, setAllCoins] = useState([]);
@@ -13,9 +15,12 @@ const AddNewAssetScreen = () => {
     allPortfolioBoughtAssetsInStorage
   );
   const [loading, setLoading] = useState(false);
-  const [selectedCoinId, setSelectedCoinId] = useState("");
+  const [selectedCoinId, setSelectedCoinId] = useState(null);
+  const [selectedCoin, setSelectedCoin] = useState(null);
 
-  const onAddNewAsset = () => {};
+  const navigation = useNavigation();
+
+  const isQuantityEntered = () => boughtAssetQuantity === "";
 
   const fetchAllCoins = async () => {
     if (loading) {
@@ -27,9 +32,42 @@ const AddNewAssetScreen = () => {
     setLoading(false);
   };
 
+  const fetchCoinInfo = async () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const coinInfo = await getDetailedCoinData(selectedCoinId);
+    setSelectedCoin(coinInfo);
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetchAllCoins();
   }, []);
+
+  useEffect(() => {
+    if (selectedCoinId) {
+      fetchCoinInfo();
+    }
+  }, [selectedCoinId]);
+
+  const onAddNewAsset = async () => {
+    const newAsset = {
+      id: selectedCoin.id,
+      name: selectedCoin.name,
+      image: selectedCoin.image.small,
+      ticker: selectedCoin.symbol.toUpperCase(),
+      quantityBought: parseFloat(boughtAssetQuantity),
+      priceBought: selectedCoin.market_data.current_price.usd,
+    };
+
+    const newAssets = [...assetsInStorage, newAsset];
+    const jsonValue = JSON.stringify(newAssets);
+    await AsyncStorage.setItem("@portfolio_coins", jsonValue);
+    setAssetsInStorage(newAssets);
+    navigation.goBack();
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -47,7 +85,7 @@ const AddNewAssetScreen = () => {
           style: styles.textInput,
         }}
       />
-      {selectedCoinId && (
+      {selectedCoin && (
         <>
           <View style={styles.boughtQuantityContainer}>
             <View style={{ flexDirection: "row" }}>
@@ -55,15 +93,33 @@ const AddNewAssetScreen = () => {
                 style={{ color: "white", fontSize: 90 }}
                 value={boughtAssetQuantity}
                 placeholder="0"
-                keyboardType="numeric"
+                // keyboardType="numeric"
                 onChangeText={setBoughtAssetQuantity}
               />
-              <Text style={styles.ticker}>BTC</Text>
+              <Text style={styles.ticker}>
+                {selectedCoin.symbol.toUpperCase()}
+              </Text>
             </View>
-            <Text style={styles.pricePerCoin}>$40000 per coin</Text>
+            <Text style={styles.pricePerCoin}>
+              {selectedCoin.market_data.current_price.usd} per coin
+            </Text>
           </View>
-          <Pressable style={styles.buttonContainer} onPress={onAddNewAsset}>
-            <Text style={styles.buttonText}>Add New Asset</Text>
+          <Pressable
+            style={{
+              ...styles.buttonContainer,
+              backgroundColor: isQuantityEntered() ? "#303030" : "#4169e1",
+            }}
+            onPress={onAddNewAsset}
+            disabled={isQuantityEntered()}
+          >
+            <Text
+              style={{
+                ...styles.buttonText,
+                color: isQuantityEntered() ? "grey" : "white",
+              }}
+            >
+              Add New Asset
+            </Text>
           </Pressable>
         </>
       )}
